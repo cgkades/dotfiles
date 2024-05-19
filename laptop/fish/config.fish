fac40b39780d7a41b125e84c08e2a9e8146a5211
cgkades <cgkades@gmail.com>
2024-05-19 09:50:25 -0600

    Laptop changes

--- laptop/fish/config.fish
+++ laptop/fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path ~/.local/bin
     alias vim="nvim"
+    alias lg=lazygit
+    alias z=zellij
 end
